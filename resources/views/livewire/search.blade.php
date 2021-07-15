4977e48a4eddca7b73ce188335f88796e6810ebe
luicahleo <luisrolandocahuanaleon@gmail.com>
2021-07-15 11:50:00 +0200

    terminado buscador

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,5 +1,5 @@
 <form class="pt-2 relative mx-auto text-gray-600" autocomplete="off">
-    <input wire:model="search" class="pr-16 mx-auto rounded"
+    <input wire:model="search" class=" border-2 border-gray-300 bg-white h-10 px-5 pr-16 mx-auto rounded text-sm "
     type="search" name="search" placeholder="Search">
 
     <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white rounded-lg font-bold absoluted">
@@ -9,17 +9,16 @@
    
     @if ($search)
         <ul class="absolute left-0 w-full bg-white mt-1 rounded-lg overflow-hidden">
-                @foreach ($this->results as $result)
+                @forelse ($this->results as $result)
                     <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300 ">{{$result->titulo}}</li>
 
-                {{--  @empty
+                    @empty
                         <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300 ">
                             No hay ninguna coincidencia :(
                         </li>
 
-                    @endempty
-                    --}}
-                @endforeach
+                    
+                @endforelse
         </ul>
     @endif
  
