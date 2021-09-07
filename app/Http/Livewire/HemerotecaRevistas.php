1419832a6bea134c63a3331e3191ad1b129dc07a
luis <luisrolandocahuanaleon@gmail.com>
2021-09-07 10:21:21 +0200

    empezando a reorganizar rutas

--- app/Http/Livewire/HemerotecaRevistas.php
+++ app/Http/Livewire/HemerotecaRevistas.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Revista;
 use Livewire\Component;
+
+//para que la paginacion sea dinamica
+use Livewire\WithPagination;
 
 class HemerotecaRevistas extends Component
 {
+    use WithPagination;
+
+    public $search;
+
     public function render()
     {
-        return view('livewire.hemeroteca-revistas');
+        $revistas = Revista::where('titulo', 'LIKE', '%' . $this->search . '%')
+                            ->orderBy('id_revista','desc')
+                            ->paginate(8);
+                            
+
+        //return $revistas;
+
+
+        return view('livewire.hemeroteca-revistas',compact('revistas'));
+    }
+
+
+    //para empezar a buscar desde la pagina 1
+    public function limpiar_page(){
+        $this->reset('page');
     }
 }
